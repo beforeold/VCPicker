d7de240422b16d337cb9279e156d8c8352d3e708
席萍萍 Xi Pingping <cirpxpp@163.com>
2017-04-10 19:48:14 +0800

    call new API

--- TestVCPicker2/AppDelegate.m
+++ TestVCPicker2/AppDelegate.m
@@ -19,7 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    [VCPicker activateWithClassPrefixesWhenDebug:@[@"MY"] needTitle:NO];
+    [VCPicker activateWithClassPrefixesWhenDebug:@[@"MY"]];
     
     return YES;
 }
